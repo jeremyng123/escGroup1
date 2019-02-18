e712d93a4105aa2262b3b4453235e23d0a4152f2
Jeremy <jeremy_ng@mymail.sutd.edu.sg>
2019-02-18 23:01:14 +0800

    Added sequelize and PG packages

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-let landing = require('../controllers/landing');
-/* GET home page. */
-router.get('/', landing.get_landing);
-router.post('/', landing.submit_lead);
-
-module.exports = router;
