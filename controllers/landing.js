848cdd236b0f026647a7ed6c01f286bda796bf34
Jeremy <jeremy_ng@mymail.sutd.edu.sg>
2019-02-19 10:13:48 +0800

    Implement /leads route
    Show a collection of leads upon submission

--- controllers/landing.js
+++ controllers/landing.js
@@ -10,6 +10,15 @@ exports.submit_lead = function(req, res, next) {
     return models.lead.create({
         email: req.body.lead_email
     }).then(lead=> {
-        res.redirect('/')
+        res.redirect('/leads')  // redirect to a new webpage when we submit email
     })
+};
+
+/* findAll() is a promise. what it means will be covered in details later.
+    This method runs asynchronously*/
+exports.show_leads = function(req, res, next) {
+    models.lead.findAll().then(leads=> {
+        res.render('landing', { title: 'Express', leads: leads });
+    })
+    
 };
