bdd019e14d9e09348634256fd1a2528a6d78898f
Jeremy <jeremy_ng@mymail.sutd.edu.sg>
2019-02-20 23:51:27 +0800

    Improve pug layout and make it more portable

--- controllers/landing.js
+++ controllers/landing.js
@@ -17,7 +17,7 @@ exports.submit_lead = function(req, res, next) {
     This method runs asynchronously*/
 exports.show_leads = function(req, res, next) {
     return models.lead.findAll().then(leads=> {
-        res.render('landing', { title: 'Express', leads: leads });
+        res.render('lead/leads', { title: 'Express', leads: leads });
     });
 };
 
@@ -27,7 +27,7 @@ exports.show_lead = function(req, res, next) {
             id : req.params.lead_id
         }
     }).then(lead => {
-        res.render('lead', { lead : lead });
+        res.render('lead/lead', { lead : lead });
     });
 };
 
