32ac728612a4e4ecdf1faf38de1a2ead1f14c753
Jeremy Ng <jeremy_ng@mymail.sutd.edu.sg>
2019-02-24 20:37:12 +0800

    just fixing some more bugs!

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
-// let user = require('../controllers/user');
-
-
-
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-=======
 let user = require('../controllers/user');
 /* Login Page */
 router.get('/login', user.show_login);
@@ -21,7 +9,6 @@ router.post('/login',user.login);
 router.post('/signup',user.signup);
 router.post('/logout', user.logout);
 router.get('/logout', user.logout);
->>>>>>> 0ab21d0070f26f50789414261215cb10f081e0fe
 
 
 module.exports = router;
