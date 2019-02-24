d94fc9d564ba5d6cb0d65480d17882a31e3f4150
Jeremy Ng <jeremy_ng@mymail.sutd.edu.sg>
2019-02-24 22:50:51 +0800

    added validation for password and fixed some other bugs

--- controllers/user.js
+++ controllers/user.js
@@ -4,32 +4,49 @@ const passport = require('passport');
 const myPassport = require('../passport_setup')(passport);
 let flash = require('connect-flash');       // this allow us to display an error message on the page that is going to be rendered. it is a one-time
 
+const { isEmpty } = require('lodash');
+const { validateUser } = require('../validators/signup');
 
+/**formData is basically the values keyed in by the user in the form fields */
 exports.show_login = function(req, res, next){
     res.render('users/login', { formData: {}, errors: {} });
 }
 exports.show_signup = function(req, res, next){
     res.render('users/signup', { formData: {}, errors: {} });
 }
+/** we use const for functions that are local (i.e. other files do not need to use this particular functions) */
+const rerender_signup = function(errors, req, res, next){
+    res.render('users/signup', { formData: req.body, errors: errors });
+}
+
 const generateHash = function(password){
     return bcrypt.hashSync(password,bcrypt.genSaltSync(8), null)
 }
 
 
 exports.signup = function(req, res, next) {
-    const newUser = models.user.build({
-        email: req.body.email,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        password: generateHash(req.body.password)
-    });
-    return newUser.save().then(result => {
-        passport.authenticate('local', {
-            successRedirect: "/",
-            failureRedirect: "/signup",
-            failureFlash: true
-        })(req, res, next);
+    let errors = {};
+    return validateUser(errors, req).then(errors => {
+        if (!isEmpty(errors)){
+            rerender_signup(errors, req, res, next)
+        }
+        else {
+            const newUser = models.user.build({
+                email: req.body.email,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                password: generateHash(req.body.password)
+            });
+            return newUser.save().then(result => {
+                passport.authenticate('local', {
+                    successRedirect: "/",
+                    failureRedirect: "/signup",
+                    failureFlash: true
+                })(req, res, next);
+            })
+        }
     })
+    
 }
 
 
---
+++ validators/signup.js
@@ -0,0 +1,53 @@
+let validator = require('validator');
+let models = require('../models');
+
+
+/** this is going to rcv an error object that is empty and it is going to populate as we find errors in each form input fields 
+ * the errors we will find is given in req field
+ * 
+
+ THIS IS A SYNCHRONOUS PIECE OF CODE
+*/
+const validateCreateUserFields = function(errors, req){
+    if (!validator.isEmail(req.body.email)){
+        errors["email"] = "Please use valid email!";
+    }
+    if (!validator.isAscii(req.body.password)){
+        errors["password"] = "Invalid characters in password, please try another one!";
+    }
+    if (!validator.isLength(req.body.password, {min: 8, max: 25})){
+        errors["password"] = "Please ensure that your password has a minimum of 8 characters";
+    }
+}
+/** we do not need to add in return email because email is already an object that will display the errors for us, if necessary, automatically */
+
+
+/**program execution:
+ * when we define our body for validateuser, when we query our database, this piece of code is an asynchronous
+ * piece of code. 
+ * 
+ * this means it will execute some time in the future, which also means we will execute the code some time in the future.
+ * 
+ * on the other hand, validateCreateUserFields is a synchronous piece of code. that means the conditions will be performed sequentially.
+ * 
+ * Our form requires both the synchronous validateCreateUserFields function, and the asynchronous validateUser functio by
+ * fusing the 2 of the functions together.
+ * 
+ * to do that, we need to use a concept called Promise, and make the entire body asynchronous
+ * to make use of both in a single function, we need to define a Promise that will define both functions in a single body.  
+ */
+exports.validateUser = function(errors, req){
+    return new Promise(function(resolve, reject){
+        validateCreateUserFields(errors,req);
+        return models.user.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then( u => {
+            if (u !== null){
+                errors["email"] = "Email is already in use. Please login using your email or reset your password"
+            }
+            resolve(errors);
+        })
+    })
+}
