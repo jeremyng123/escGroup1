9df6d738a5e21816f0c802575f9f223427aa718b
Jeremy Ng <jeremy_ng@mymail.sutd.edu.sg>
2019-03-21 21:14:13 +0800

    realised that the env is PRODUCTION, so made some changes... fingers crossed

--- config/config.js
+++ config/config.js
@@ -15,10 +15,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": "postgres",
+    "port": 5432
   }
 }
