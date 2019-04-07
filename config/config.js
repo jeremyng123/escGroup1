2658dd10e59df25a2d100b4f36f560b7a33c7fce
Chelsea Foo <chelseafoo@localhost.localdomain>
2019-04-08 01:58:49 +0800

    init commit for integration

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  "development": {
-    "username": "express-mvp-dbuser",
-    "password": '123.456',
-    "database": "express-mvp-db",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "port" : 5432
+  development: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    port: 5432
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql"
   }
-}
+};
