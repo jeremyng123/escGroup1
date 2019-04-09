2cb950ee46ae4f5fce1fdaf920dcc968cb701a14
Emrys-Hong <hongpengfei.emrys@gmail.com>
2019-04-09 23:50:57 +0800

    reorder app.js and exclude express() from it

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -7,50 +6,49 @@ let passport = require('passport');
 let session = require('express-session');
 let flash = require('connect-flash'); 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 require('./passport_setup')(passport);
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+module.exports = function(app, io) {
+  var indexRouter = require('./routes/index')(io);
+  var usersRouter = require('./routes/users');
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-/******************* use Session **************************/
-// take note that we have to write the following codes in order
-// bcus nodejs read codes line by line
-app.use(session({ secret: 'our little secret' }));
-app.use(passport.initialize());   // we want to set up passport in a separate file from app.js. for that, we add in the line at line 12
-app.use(passport.session());
+  /******************* use Session **************************/
+  // take note that we have to write the following codes in order
+  // bcus nodejs read codes line by line
+  app.use(session({ secret: 'our little secret' }));
+  app.use(passport.initialize());   // we want to set up passport in a separate file from app.js. for that, we add in the line at line 12
+  app.use(passport.session());
 
-/** if user is not logged in, direct to usersRouter */
-// app.all('/secret', isLoggedIn)
-app.use(flash());
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  /** if user is not logged in, direct to usersRouter */
+  // app.all('/secret', isLoggedIn)
+  app.use(flash());
+  app.use('/', indexRouter);
+  app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
-module.exports = app;
+}
