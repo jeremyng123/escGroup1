e1bced139217e55f48e3ed4fca58a1c65aa306a2
jeremyng123 <jeremy_ng@mymail.sutd.edu.sg>
2019-04-11 21:56:55 +0800

    render HTML-texts and images

--- routes/index.js
+++ routes/index.js
@@ -1,82 +1,27 @@
-var express = require('express');
-
-
-/*************** CONTROLLER *****************/
-let general = require('../controllers/general');
-let admins = require('../controllers/admins');
-let user = require('../controllers/user');      // to direct them to login page!
-let rtchat = require('../controllers/chat');
-
-/*************** MIDDLEWARE *****************/
-let {isLoggedIn,hasAuth,whatRights} = require('../middleware/hasAuth');
-let {send_email} = require('../middleware/email');
-
-/*************** UPLOAD IMAGES *****************/
-// let {uploadImage} = require('../controllers/upload');
-var multer = require('multer');
-var path = require('path');
-
-/***
- * import fs
- */
-var fs = require('fs')
-
-/**
- * Use the gravatar module, to turn email addresses into avatar images:  
- * for REAL TIME CHAT
- * */
-var gravatar = require('gravatar');
-
-var storage = multer.diskStorage({
-  destination: function(req, file, cb, res) {
-    cb(null, 'public/users/' + req.user.userId + "/tickets/" + req.user.ticketCount);
-  },
-
-  filename: function(req, file, cb, res) {
-    var name = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
-    cb(null, name);
-
-    return name;
-  }
-});
-var upload = multer({
-  storage: storage
-});
-
-
-
-function checkUploadPath(req, res, next) {
-    const uploadPath = 'public/users/' + req.user.userId + "/tickets/" + req.user.ticketCount;
-    fs.exists(uploadPath, function(exists) {
-        if(exists) {
-          return next();
-        }
-        else {
-            fs.mkdir(uploadPath, { recursive: true }, function(err) {
-            if(err) {
-                console.log('Error in folder creation: \n' + err);
-                return next(); 
-            }  
-                return next();
-            })
-        }
-    })
-}
-
-
-
-module.exports = function(io) {
+module.exports = function(io){
+  var express = require('express');
   var router = express.Router();
-  router.post('/upload', checkUploadPath, upload.single('file'), function(req, res) {
 
-      res.json({
-          "location": '/users/' + req.user.userId + "/tickets/" + req.user.ticketCount + "/" + req.file.filename
-      });
-      
-  });
+  /*************** CONTROLLER *****************/
+  let general = require('../controllers/general');
+  let admins = require('../controllers/admins');
+  let user = require('../controllers/user');      // to direct them to login page!
+
+
+  /*************** MIDDLEWARE *****************/
+  let {isLoggedIn,hasAuth,whatRights} = require('../middleware/hasAuth');
+  let {send_email} = require('../middleware/email');
+
+  /*************** REAL TIME CHAT DEPENDENCIES *****************/
+  /**
+   * Use the gravatar module, to turn email addresses into avatar images:  
+   * for REAL TIME CHAT
+   * */
+  let rtchat = require('../controllers/chat');
+  var gravatar = require('gravatar');
 
   /*************** HOMEPAGE *****************/
-  router.get('/',send_email, general.get_welcome);
+  router.get('/', general.get_welcome);
 
     /*************** GENERAL TICKET ROUTES *****************/
   router.get('/my_tickets/:user_id/0', isLoggedIn, general.show_my_tickets_queued);       // user page -- display all queued tickets
@@ -105,49 +50,64 @@ module.exports = function(io) {
   router.post('/ticket/:ticket_id/delete', isLoggedIn, hasAuth, admins.delete_ticket);       // using post and different route
   router.post('/ticket/:ticket_id/delete-json',isLoggedIn, hasAuth, admins.delete_ticket_json);  // using ajax
 
-
-
-  /*************** UPLOAD IMAGES *****************/
-  // var multer = require('multer');
-  // var path = require('path');
-  // var mkdirp = require('mkdirp');
-
-  // var storage = multer.diskStorage({
-  //   destination: function(req, file, cb, res) {
-  //     cb(null, 'public/static/dist/uploads');
-  //   },
-
-  //   filename: function(req, file, cb, res) {
-  //     var name = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
-  //     cb(null, name);
-  //     return name;
-  //   }
-  // });
-  // var upload = multer({
-  //   storage: storage
-  // });
-
-  // router.post('/upload', function(req,res,next){
-  //     mkdirp('./public/users/' + req.params.user_id + "/tickets/" + req.params.ticket_id, function(err){
-  //         if (err) console.log("Error in making directory! " + err.message);
-  //     }),
-          
-  //     upload.single('file'), function(req,res,next){
-  //         res.json({
-  //             "location": '/users/' + req.params.user_id + "/tickets/" + req.params.ticket_id + "/" + req.file.filename
-  //         });
-  //     }
-  // })
-
-  /***************** 
-   * REAL TIME CHAT 
-   * ****************/
-
+  /********* REAL TIME CHAT ROUTER *************/
   router.get('/room', rtchat.room);
   router.get('/create', rtchat.create);
   router.get('/chat/:id', rtchat.chat);
 
 
+  /*************** UPLOAD IMAGES *****************/
+  var multer = require('multer');
+  var path = require('path');
+  var fs = require('fs')
+
+  var storage = multer.diskStorage({
+    destination: function(req, file, cb, res) {
+      cb(null, 'public/users/' + req.user.userId + "/tickets/" + req.user.ticketCount);
+    },
+
+    filename: function(req, file, cb, res) {
+      var name = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
+      cb(null, name);
+
+      return name;
+    }
+  });
+  var upload = multer({
+    storage: storage
+  });
+
+  function checkUploadPath(req, res, next) {
+      const uploadPath = 'public/users/' + req.user.userId + "/tickets/" + req.user.ticketCount;
+      fs.exists(uploadPath, function(exists) {
+          if(exists) {
+            return next();
+          }
+          else {
+              fs.mkdir(uploadPath, { recursive: true }, function(err) {
+              if(err) {
+                  console.log('Error in folder creation: \n' + err);
+                  return next(); 
+              }  
+                  return next();
+              })
+          }
+      })
+  }
+
+  router.post('/upload', checkUploadPath, upload.single('file'), function(req, res) {
+      res.json({
+          "location": 'users/' + req.user.userId + "/tickets/" + req.user.ticketCount + "/" + req.file.filename
+      });
+  });
+
+
+  /***************** 
+   * REAL TIME CHAT 
+   * ****************/
+
+
+  
   // Initialize a new socket.io application, named 'chat'
   var chat = io.on('connection', function (socket) {
 
@@ -270,16 +230,5 @@ module.exports = function(io) {
     }
     return res;
   }
-
-
-
-
-  // this is the key
   return router;
-
-
-
-
-
-
 }
