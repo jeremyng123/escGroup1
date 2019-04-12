6ce57fdb289f565e154d88defb2c1b3ac8851fae
jeremyng123 <jeremy_ng@mymail.sutd.edu.sg>
2019-04-12 11:16:57 +0800

    some changes to heroku...

--- tests/test_no-user.js
+++ tests/test_no-user.js
@@ -66,6 +66,7 @@ describe('Title pages for non-users', function() {
             await driver.findElement(By.name('lastName')).sendKeys('Ng');         // enter default user last name into `lastName` box
             await driver.findElement(By.name('phoneNumber')).sendKeys(phoneNumber);         // enter default user phonenumber into `phoneNumber` box
             await driver.findElement(By.name('password')).sendKeys('1234567', Key.ENTER);         // enter short password into `password` box
+            await driver.wait(until.elementLocated(By.id('err_password')));
             try{
                 await driver.findElement(By.id('err_password')).then(err_pw => {
                     console.log('invalid length of password: \n\n\n' + err_pw);
