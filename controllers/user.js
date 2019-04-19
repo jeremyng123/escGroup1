b38def1e38c759399144cb0aaf1bb8a2b177f99a
jeremyng123 <jeremy_ng@mymail.sutd.edu.sg>
2019-04-19 14:16:58 +0800

    fixed the ui for forgot password

--- controllers/user.js
+++ controllers/user.js
@@ -54,7 +54,7 @@ exports.show_forgot_password = function(req, res, next) {
  */
 function makeid(length) {
 	var text = "";
-	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()][';/.,|}{:?><`~";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()][';/.,|}{:?>`~";
   
 	for (var i = 0; i < length; i++)
 	  text += possible.charAt(Math.floor(Math.random() * possible.length));
