1665bbac0ff12cbf13759951fffba0f982603246
Jeremy Ng <jeremy_ng@mymail.sutd.edu.sg>
2019-04-20 22:57:04 +0800

    fixed some stuff

--- controllers/admins.js
+++ controllers/admins.js
@@ -44,9 +44,9 @@ exports.post_message = function(req, res, next) {
     const TICKET_PAGE = "/tickets/" + req.user.userId +"/" + req.params.ticket_id;
     return models.ticket.findOne({
         where   : { ticketId : req.params.ticket_id },
-        include : [models.user , models.message ]
+        include : [ models.message ]
     }).then (ticket => {
-        if (ticket){
+        if (ticket.tag !== 1){
             ticket.update({
                 tag :   1
             }).then( () => {
@@ -58,10 +58,20 @@ exports.post_message = function(req, res, next) {
                     fullName    : FULLNAME,
                     email       : req.user.email,
                     phoneNumber : req.user.phoneNumber
-                }).then(() => { res.redirect(TICKET_PAGE); }).catch(err => { new Error("i cannot create message")})
-            })
+                }).then(() => { res.redirect(TICKET_PAGE); })
+            }).catch(err => { new Error("i cannot create message")})
+        } else {
+                return models.message.create({
+                    fk_userId   : req.user.userId,
+                    fk_ticketId : ticket.ticketId,
+                    content     : req.body.content,
+                    is_admin    : req.user.is_admin,
+                    fullName    : FULLNAME,
+                    email       : req.user.email,
+                    phoneNumber : req.user.phoneNumber
+                }).then(() => { res.redirect(TICKET_PAGE); })
         }
-    })
+    }).catch(err => { new Error (err, "Unable to post message...")})
 }
 
 exports.delete_ticket = function(req, res, next) {
--- public/javascripts/ticket_table.js
+++ public/javascripts/ticket_table.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
 
     $('.table-hover tr').click(function() {
         href = $(this).attr('href');
-        alert(window.location.host + href);
+        // alert(window.location.host + href);
 
         window.location.href = href;
     });
