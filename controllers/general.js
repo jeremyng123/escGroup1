c9ec30283a8ddfd602999d60b9aa74f9e5f3f238
Emrys-Hong <hongpengfei.emrys@gmail.com>
2019-04-22 05:14:51 +0800

    add welcome admin route

--- controllers/general.js
+++ controllers/general.js
@@ -13,11 +13,18 @@ exports.get_welcome = function(req, res, next) {
 };
 
 exports.get_welcome_admin = function(req, res, next) {
-  return res.render("welcomeAdmin", {
-    title: "Accenture's ACNAPI Portal",
-    user: req.user
+  return models.ticket.findAll({
+    where : { tag : 0 },
+    include: [ models.user ]
+  }).then(
+    tickets => {
+      res.render("welcomeAdmin", {
+        title: "Accenture's ACNAPI Portal",
+        user: req.user, tickets: tickets
+      });
   });
 }
+
 exports.get_consultantpage = function(req, res, next) {
   return res.render("consultant", {
     title: "Accenture's ACNAPI Portal",
