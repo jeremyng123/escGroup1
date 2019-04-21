0bc163518bbfe4d5aa404af716864ab572132222
Jeremy Ng <jeremy_ng@mymail.sutd.edu.sg>
2019-04-21 23:48:59 +0800

    signup and navbar/welcome page fixed

--- public/javascripts/ticket_table.js
+++ public/javascripts/ticket_table.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
 
     $('.table.table-hover tr').click(function() {
         href = $(this).attr('href');
-        alert(window.location.host + href);
+        // alert(window.location.host + href);
 
         window.location.replace(href);
     });
